88ad6b4446d390f2c99806db0513e206e691118a
movies4uonline <moviesu537@gmail.com>
2026-06-16 01:32:06 +0530

    Update movies.js

--- movies.js
+++ movies.js
@@ -8,31 +8,4 @@ const allMovies = [
         "rating": "9.0",
         "lang": "MAL"
     },
-    {
-        "id": 2,
-        "title": "Pushpa 2: The Rule",
-        "year": "2024",
-        "img": "https://i.ibb.co/3Yqm4pN/lcubg.jpg",
-        "link": "https://sample-videos.com/video321.mp4",
-        "rating": "9.5",
-        "lang": "MAL"
-    },
-    {
-        "id": 3,
-        "title": "Leo",
-        "year": "2023",
-        "img": "https://upload.wikimedia.org/wikipedia/en/b/b3/Leo_%282023_Tamil_film%29.jpg",
-        "link": "https://sample-videos.com/video789.mp4",
-        "rating": "8.5",
-        "lang": "TAM"
-    },
-    {
-        "id": 4,
-        "title": "Turbo",
-        "year": "2024",
-        "img": "https://upload.wikimedia.org/wikipedia/en/f/f7/Turbo_2024_film_poster.jpg",
-        "link": "https://sample-videos.com/video456.mp4",
-        "rating": "8.2",
-        "lang": "MAL"
-    }
 ];
