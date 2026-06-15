705422a610b8cef7d2ac0ae8c3f0f8847ab6aed9
movies4uonline <moviesu537@gmail.com>
2026-06-16 01:18:42 +0530

    Change movie image and link in movies.js
    Updated image and link for the movie 'Aavesham'.

--- movies.js
+++ movies.js
@@ -3,8 +3,8 @@
         "id": 1,
         "title": "Aavesham",
         "year": "2024",
-        "img": "https://upload.wikimedia.org/wikipedia/en/3/30/Aavesham_Poster.jpg",
-        "link": "https://sample-videos.com/video123.mp4",
+        "img": "https://image.tmdb.org/t/p/original/gsXz7wmbqQ8ywNjJhlpOkxMtOZ4.jpg",
+        "link": "https://watchmovies4u.movies4uu.workers.dev/dl/1h9zhDkXTnMinD9NV93H2hUx33M7Lhx5Ho5x9oUjldlra24ne2Lh6dUDiRh4EwuNTAdAyH72r2FV/Pongala%20(2025)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.5GB%20-%20ESub.mkv",
         "rating": "9.0",
         "lang": "MAL"
     },
