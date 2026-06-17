1c0c445d7da496396783e90d3b03f2f3375bc279
movies4uonline <moviesu537@gmail.com>
2026-06-18 03:25:31 +0530

    Update movies.js

--- movies.js
+++ movies.js
@@ -2,12 +2,12 @@
   {
     "title": "Karuppu",
     "img": "karuppu",
+    "year": "2024",
+    "lang": "Malayalam",
     "link1080": "https://watchmovies4u.movies4uu.workers.dev/dl/1h9zhDkXTnMinD9NV93H2hUx33M7Lhx5Ho5x9oUjldlra24ne2Lh6dUDiRh4EwuNTAdAyH72r2FV/Pongala%20(2025)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.5GB%20-%20ESub.mkv",
     "link720": "https://watchmovies4u.movies4uu.workers.dev/dl/1h9zhDkXTnMinD9NV93H2hUx33M7Lhx5Ho5x9oUjldlra24ne2Lh6dUDiRh4EwuNTAdAyH72r2FV/Pongala%20(2025)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.5GB%20-%20ESub.mkv",
     "link480": "https://watchmovies4u.movies4uu.workers.dev/dl/1h9zhDkXTnMinD9NV93H2hUx33M7Lhx5Ho5x9oUjldlra24ne2Lh6dUDiRh4EwuNTAdAyH72r2FV/Pongala%20(2025)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.5GB%20-%20ESub.mkv",
-    "year": "2024",
-    "lang": "Malayalam",
-    "quality": "HD"
+    "telegram": ""
   },
    {
     "title": "Pongala",
