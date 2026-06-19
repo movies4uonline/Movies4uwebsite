bd777f5195abecf1e82664bc8b0e5cb3b2623c4a
movies4uonline <moviesu537@gmail.com>
2026-06-19 07:45:19 +0530

    Update movies.js

--- movies.js
+++ movies.js
@@ -10,76 +10,7 @@
     "link480": "https://watchmovies4u.movies4uu.workers.dev/dl/1h9zhDkXTnMinD9NV93H2hUx33M7Lhx5Ho5x9oUjldlra24ne2Lh6dUDiRh4EwuNTAdAyH72r2FV/Pongala%20(2025)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.5GB%20-%20ESub.mkv",
     "telegram": ""
   },
-   {
-    "title": "Pongala",
-    "img": "pongala",
-    "link1080": "https://watchmovies4u.movies4uu.workers.dev/dl/1h9zhDkXTnMinD9NV93H2hUx33M7Lhx5Ho5x9oUjldlra24ne2Lh6dUDiRh4EwuNTAdAyH72r2FV/Pongala%20(2025)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.5GB%20-%20ESub.mkv",
-    "link720": "https://watchmovies4u.movies4uu.workers.dev/dl/1h9zhDkXTnMinD9NV93H2hUx33M7Lhx5Ho5x9oUjldlra24ne2Lh6dUDiRh4EwuNTAdAyH72r2FV/Pongala%20(2025)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.5GB%20-%20ESub.mkv",
-    "link480": "https://watchmovies4u.movies4uu.workers.dev/dl/1h9zhDkXTnMinD9NV93H2hUx33M7Lhx5Ho5x9oUjldlra24ne2Lh6dUDiRh4EwuNTAdAyH72r2FV/Pongala%20(2025)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.5GB%20-%20ESub.mkv",
-    "year": "2024",
-    "lang": "Malayalam",
-    "quality": "4K ULTRA HD"
-   },
-  {
-    "title": "Dridam",
-    "img": "dridam",
-    "link1080": "https://watchmovies4u.movies4uu.workers.dev/dl/1h9zhDkXTnMinD9NV93H2hUx33M7Lhx5Ho5x9oUjldlra24ne2Lh6dUDiRh4EwuNTAdAyH72r2FV/Pongala%20(2025)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.5GB%20-%20ESub.mkv",
-    "link720": "https://watchmovies4u.movies4uu.workers.dev/dl/1h9zhDkXTnMinD9NV93H2hUx33M7Lhx5Ho5x9oUjldlra24ne2Lh6dUDiRh4EwuNTAdAyH72r2FV/Pongala%20(2025)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.5GB%20-%20ESub.mkv",
-    "link480": "https://watchmovies4u.movies4uu.workers.dev/dl/1h9zhDkXTnMinD9NV93H2hUx33M7Lhx5Ho5x9oUjldlra24ne2Lh6dUDiRh4EwuNTAdAyH72r2FV/Pongala%20(2025)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.5GB%20-%20ESub.mkv",
-    "year": "2024",
-    "lang": "Malayalam",
-    "quality": "4K ULTRA HD"
-  },
-  {
-    "title": "Aashaan",
-    "img": "aashaan",
-    "link1080": "https://watchmovies4u.movies4uu.workers.dev/dl/1h9zhDkXTnMinD9NV93H2hUx33M7Lhx5Ho5x9oUjldlra24ne2Lh6dUDiRh4EwuNTAdAyH72r2FV/Pongala%20(2025)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.5GB%20-%20ESub.mkv",
-    "link720": "https://watchmovies4u.movies4uu.workers.dev/dl/1h9zhDkXTnMinD9NV93H2hUx33M7Lhx5Ho5x9oUjldlra24ne2Lh6dUDiRh4EwuNTAdAyH72r2FV/Pongala%20(2025)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.5GB%20-%20ESub.mkv",
-    "link480": "https://watchmovies4u.movies4uu.workers.dev/dl/1h9zhDkXTnMinD9NV93H2hUx33M7Lhx5Ho5x9oUjldlra24ne2Lh6dUDiRh4EwuNTAdAyH72r2FV/Pongala%20(2025)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.5GB%20-%20ESub.mkv",
-    "year": "2024",
-    "lang": "Malayalam",
-    "quality": "4K ULTRA HD"
-  },
-  {
-    "title": "Aavesham",
-    "img": "https://image.tmdb.org/t/p/original/gsXz7wmbqQ8ywNjJhlpOkxMtOZ4.jpg",
-    "link1080": "https://watchmovies4u.movies4uu.workers.dev/dl/1h9zhDkXTnMinD9NV93H2hUx33M7Lhx5Ho5x9oUjldlra24ne2Lh6dUDiRh4EwuNTAdAyH72r2FV/Pongala%20(2025)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.5GB%20-%20ESub.mkv",
-    "link720": "https://watchmovies4u.movies4uu.workers.dev/dl/1h9zhDkXTnMinD9NV93H2hUx33M7Lhx5Ho5x9oUjldlra24ne2Lh6dUDiRh4EwuNTAdAyH72r2FV/Pongala%20(2025)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.5GB%20-%20ESub.mkv",
-    "link480": "https://watchmovies4u.movies4uu.workers.dev/dl/1h9zhDkXTnMinD9NV93H2hUx33M7Lhx5Ho5x9oUjldlra24ne2Lh6dUDiRh4EwuNTAdAyH72r2FV/Pongala%20(2025)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.5GB%20-%20ESub.mkv",
-    "year": "2024",
-    "lang": "Tamil",
-    "quality": "4K ULTRA HD"
-  },
-   {
-    "title": "Aavesham",
-    "img": "https://image.tmdb.org/t/p/original/gsXz7wmbqQ8ywNjJhlpOkxMtOZ4.jpg",
-    "link1080": "https://watchmovies4u.movies4uu.workers.dev/dl/1h9zhDkXTnMinD9NV93H2hUx33M7Lhx5Ho5x9oUjldlra24ne2Lh6dUDiRh4EwuNTAdAyH72r2FV/Pongala%20(2025)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.5GB%20-%20ESub.mkv",
-    "link720": "https://watchmovies4u.movies4uu.workers.dev/dl/1h9zhDkXTnMinD9NV93H2hUx33M7Lhx5Ho5x9oUjldlra24ne2Lh6dUDiRh4EwuNTAdAyH72r2FV/Pongala%20(2025)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.5GB%20-%20ESub.mkv",
-    "link480": "https://watchmovies4u.movies4uu.workers.dev/dl/1h9zhDkXTnMinD9NV93H2hUx33M7Lhx5Ho5x9oUjldlra24ne2Lh6dUDiRh4EwuNTAdAyH72r2FV/Pongala%20(2025)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.5GB%20-%20ESub.mkv",
-    "year": "2024",
-    "lang": "Tamil",
-    "quality": "4K ULTRA HD"
-   },
-   {
-    "title": "Aavesham",
-    "img": "https://image.tmdb.org/t/p/original/gsXz7wmbqQ8ywNjJhlpOkxMtOZ4.jpg",
-    "link1080": "https://watchmovies4u.movies4uu.workers.dev/dl/1h9zhDkXTnMinD9NV93H2hUx33M7Lhx5Ho5x9oUjldlra24ne2Lh6dUDiRh4EwuNTAdAyH72r2FV/Pongala%20(2025)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.5GB%20-%20ESub.mkv",
-    "link720": "https://watchmovies4u.movies4uu.workers.dev/dl/1h9zhDkXTnMinD9NV93H2hUx33M7Lhx5Ho5x9oUjldlra24ne2Lh6dUDiRh4EwuNTAdAyH72r2FV/Pongala%20(2025)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.5GB%20-%20ESub.mkv",
-    "link480": "https://watchmovies4u.movies4uu.workers.dev/dl/1h9zhDkXTnMinD9NV93H2hUx33M7Lhx5Ho5x9oUjldlra24ne2Lh6dUDiRh4EwuNTAdAyH72r2FV/Pongala%20(2025)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.5GB%20-%20ESub.mkv",
-    "year": "2024",
-    "lang": "Tamil",
-    "quality": "4K ULTRA HD"
-   },
-   {
-    "title": "Aavesham",
-    "img": "https://image.tmdb.org/t/p/original/gsXz7wmbqQ8ywNjJhlpOkxMtOZ4.jpg",
-    "link1080": "https://watchmovies4u.movies4uu.workers.dev/dl/1h9zhDkXTnMinD9NV93H2hUx33M7Lhx5Ho5x9oUjldlra24ne2Lh6dUDiRh4EwuNTAdAyH72r2FV/Pongala%20(2025)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.5GB%20-%20ESub.mkv",
-    "link720": "https://watchmovies4u.movies4uu.workers.dev/dl/1h9zhDkXTnMinD9NV93H2hUx33M7Lhx5Ho5x9oUjldlra24ne2Lh6dUDiRh4EwuNTAdAyH72r2FV/Pongala%20(2025)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.5GB%20-%20ESub.mkv",
-    "link480": "https://watchmovies4u.movies4uu.workers.dev/dl/1h9zhDkXTnMinD9NV93H2hUx33M7Lhx5Ho5x9oUjldlra24ne2Lh6dUDiRh4EwuNTAdAyH72r2FV/Pongala%20(2025)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.5GB%20-%20ESub.mkv",
-    "year": "2024",
-    "lang": "Tamil",
-    "quality": "4K ULTRA HD"
-   },
+  
   {
     "title": "Asianet",
     "link": "http://217.20.112.199/asianet/index.m3u8",
