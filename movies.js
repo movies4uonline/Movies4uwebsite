79eef7631e4f71d5a9c3fe6dd48e36876ed15d7c
movies4uonline <moviesu537@gmail.com>
2026-06-19 07:51:09 +0530

    Update movies.js

--- movies.js
+++ movies.js
@@ -6,8 +6,8 @@
     "lang": "Malayalam",
     "rating": "4.4",
     "link1080": "https://watchmovies4u.movies4uu.workers.dev/dl/1h9zhDkXTnMinD9NV93H2hUx33M7Lhx5Ho5x9oUjldlra24ne2Lh6dUDiRh4EwuNTAdAyH72r2FV/Pongala%20(2025)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.5GB%20-%20ESub.mkv",
-    "link720": "https://watchmovies4u.movies4uu.workers.dev/dl/1h9zhDkXTnMinD9NV93H2hUx33M7Lhx5Ho5x9oUjldlra24ne2Lh6dUDiRh4EwuNTAdAyH72r2FV/Pongala%20(2025)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.5GB%20-%20ESub.mkv",
-    "link480": "https://watchmovies4u.movies4uu.workers.dev/dl/1h9zhDkXTnMinD9NV93H2hUx33M7Lhx5Ho5x9oUjldlra24ne2Lh6dUDiRh4EwuNTAdAyH72r2FV/Pongala%20(2025)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.5GB%20-%20ESub.mkv",
+    "link720": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qrqvNpFf3GcfVspavhu5qMiAPko4fHit/Karuppu%20(2026)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps)%20-%201.2GB%20-%20ESub.mkv",
+    "link480": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qrqqVLMu3grSeMBPnrYJdCfeE7NHZt1D/Karuppu%20(2026)%20Malayalam%20HQ%20HDRip%20-%20x264%20-%20AAC%20-%20400MB%20-%20ESub.mkv",
     "telegram": ""
   },
   
