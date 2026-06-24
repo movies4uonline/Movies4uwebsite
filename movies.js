b963f2fc82f742b556dd2eb4f76b734dca8dfd74
movies4uonline <moviesu537@gmail.com>
2026-06-25 03:30:47 +0530

    Update movies.js

--- movies.js
+++ movies.js
@@ -1,39 +1,31 @@
- [
-    {
-        "title": "Blast",
-        "img": "blast",
-        "year": "2026",
-        "lang": "Malayalam",
-        "rating": "7.8",
-        
-        
-        "link1080": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qhdL8QvIxYprnDP3vXmng3hbcxDCdkLo/Blast%20(2026)%20Malayalam%20TRUE%20WEB-DL%20-%201080p%20-%20AVC%20-%20UNTOUCHED%20-%20(DD%2B5.1%20-%20192Kbps)%20-%202.2GB%20-%20ESub.mkv",
-        "name1080": "Blast (2026) Malayalam TRUE WEB-DL - 1080p - AVC - UNTOUCHED - (DD+5.1 - 192Kbps) - 2.2GB - ESub", 
-        "size1080": "2.2 GB",
-        
-        "link720": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qhdGhcCXxyF3GY1Io7PvyuVrW3myjONo/Blast%20(2026)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps)%20-%201.5GB%20-%20ESub.mkv",
-        "name720": "Blast (2026) Malayalam HQ HDRip - 720p - x264 - (DD+5.1 - 192Kbps) - 1.5GB - ESub",
-        "size720": "1.5 GB",
-        
-        "link480": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qhdII1zl2LxfxiVnX2zqNtC0cF6OWCFh/Blast%20(2026)%20Malaylam%20HQ%20HDRip%20-%20x264%20-%20AAC%20-%20400MB%20-%20ESub.mkv",
-        "name480": "Blast (2026) Malaylam HQ HDRip - x264 - AAC - 400MB - ESub",
-        "size480": "400 MB",
-
-        
-        "ext_link1080": "https://lcuapi.onlinewatch.workers.dev/api/fdl/AgAD2S4786793",
-        "ext_name1080": "Blast (2026) Malayalam TRUE WEB-DL - 1080p - AVC - UNTOUCHED - (DD+5.1 - 192Kbps) - 2.2GB - ESub",
-        "ext_size1080": "2.2 GB",
-        
-        "ext_link720": "https://lcuapi.onlinewatch.workers.dev/api/fdl/AgAD1y4786791",
-        "ext_name720": "Blast (2026) Malayalam HQ HDRip - 720p - x264 - (DD+5.1 - 192Kbps) - 1.5GB - ESub",
-        "ext_size720": "1.5 GB",
-        
-        "ext_link480": "https://lcuapi.onlinewatch.workers.dev/api/fdl/AgADyS4786790",
-        "ext_name480": "Blast (2026) Malaylam HQ HDRip - x264 - AAC - 400MB - ESub",
-        "ext_size480": "400 MB",
-
-        "telegram": "https://t.me/your_channel"
-    },
+const allMovies = [
+  {
+    "title": "Blast",
+    "img": "blast",
+    "year": "2026",
+    "lang": "Malayalam",
+    "rating": "7.8",
+    "link1080": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qhdL8QvIxYprnDP3vXmng3hbcxDCdkLo/Blast%20(2026)%20Malayalam%20TRUE%20WEB-DL%20-%201080p%20-%20AVC%20-%20UNTOUCHED%20-%20(DD%2B5.1%20-%20192Kbps)%20-%202.2GB%20-%20ESub.mkv",
+    "name1080": "Blast (2026) Malayalam TRUE WEB-DL - 1080p",
+    "size1080": "2.2 GB",
+    "link720": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qhdGhcCXxyF3GY1Io7PvyuVrW3myjONo/Blast%20(2026)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps)%20-%201.5GB%20-%20ESub.mkv",
+    "name720": "Blast (2026) Malayalam HQ HDRip - 720p",
+    "size720": "1.5 GB",
+    "link480": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qhdII1zl2LxfxiVnX2zqNtC0cF6OWCFh/Blast%20(2026)%20Malaylam%20HQ%20HDRip%20-%20x264%20-%20AAC%20-%20400MB%20-%20ESub.mkv",
+    "name480": "Blast (2026) Malaylam HQ HDRip - 480p",
+    "size480": "400 MB",
+    "ext_link1080": "https://lcuapi.onlinewatch.workers.dev/api/fdl/AgAD2S4786793",
+    "ext_name1080": "Download 1080p [High Quality]",
+    "ext_size1080": "2.2 GB",
+    "ext_link720": "https://lcuapi.onlinewatch.workers.dev/api/fdl/AgAD1y4786791",
+    "ext_name720": "Download 720p [Standard]",
+    "ext_size720": "1.5 GB",
+    "ext_link480": "https://lcuapi.onlinewatch.workers.dev/api/fdl/AgADyS4786790",
+    "ext_name480": "Download 480p [Data Saver]",
+    "ext_size480": "400 MB",
+    "telegram": "https://t.me/your_channel",
+    "player_title": "Blast (2026) Malayalam Official"
+  },
   {
     "title": "Gu (2024) Malayalam HDRip",
     "year": "2024",
@@ -43,8 +35,9 @@
     "link1080": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qrgt34CccwrlPaxykTExBbfaLsJrGFey/Gu%20(2024)%20Malayalam%20HQ%20HDRip%20-%201080p%20-%20x264%20-%20(DD%2B5.1%20-%20384Kbps%20%26%20AAC%202.0)%20-%202.8GB%20-%20ESub.mkv",
     "link720": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qrgobqJrc6GY8uKdd47FzN1NTbkCkcUA/Gu%20(2024)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.4GB%20-%20ESub.mkv",
     "link480": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qBDljCHwzNJs9WMCCrbGlkmpFxQxPm5b/Gu%20(2024)%20Malayalam%20HQ%20HDRip%20-%20x264%20-%20AAC%20-%20400MB%20-%20ESub.mkv",
-    "link_server2": "",
-    "link_server3": "https://download-server.com/file.mp4",
+    "size1080": "2.8 GB",
+    "size720": "1.4 GB",
+    "size480": "400 MB",
     "telegram": "https://t.me/Movies4U"
   },
   {
@@ -56,29 +49,10 @@
     "link1080": "https://watchmovies4u.movies4uu.workers.dev/dl/1h9zhDkXTnMinD9NV93H2hUx33M7Lhx5Ho5x9oUjldlra24kNdOTB12rswnp5BJneRNbHzvNAJTn/Athiradi%20(2026)%20Malayalam%20HQ%20HDRip%20-%201080p%20-%20x264%20-%20(DD%2B5.1%20ATMOS%20-%20448Kbps%20%26%20AAC%202.0)%20-%203.2GB%20-%20ESub.mkv",
     "link720": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qBDiSNL94ARgjBSX3woKf3J44lKLYYEm/Athiradi%20(2026)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20256Kbps%20%26%20AAC%202.0)%20-%201.6GB%20-%20ESub.mkv",
     "link480": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qBDe19So4b6SCW5BVGRSaOz2BDDK7WXM/Athiradi%20(2026)%20Malayalam%20HQ%20HDRip%20-%20x264%20-%20AAC%20-%20400MB%20-%20ESub.mkv",
-    "telegram": ""
-  },
-  {
-    "title": "Drishyam 3",
-    "img": "drishyam3",
-    "year": "2026",
-    "lang": "Malayalam",
-    "rating": "7.4",
-    "link1080": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qgTFEapCPETDb45PbbCKu1zOTDejUroz/Drishyam%203%20(2026)%20Malayalam%20TRUE%20WEB-DL%20-%201080p%20-%20AVC%20-%20UNTOUCHED%20-%20(DD%2B5.1%20-%20640Kbps%20%26%20AAC%202.0)%20-%202.5GB%20-%20ESub.mkv",
-    "link720": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qgTDdltfks1rkJc9CgSPlMzy2AaK5C0B/Drishyam%203%20(2026)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.4GB%20-%20ESub.mkv",
-    "link480": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qgTAMwwRPf9fuoiu3m5SIWnP44E43j7s/Drishyam%203%20(2026)%20Malayalam%20HQ%20HDRip%20-%20x264%20-%20AAC%20-%20400MB%20-%20ESub.mkv",
-    "telegram": ""
-  },
-  {
-    "title": "Karuppu",
-    "img": "karuppu",
-    "year": "2026",
-    "lang": "Malayalam",
-    "rating": "6.4",
-    "link1080": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qgTylHAuk2h3E3oOurkrNWP9HHXS6NTR/Karuppu%20(2026)%20Malayalam%20TRUE%20WEB-DL%20-%201080p%20-%20AVC%20-%20UNTOUCHED%20-%20(DD%2B5.1%20-%20192Kbps)%20-%202.3GB%20-%20ESub.mkv",
-    "link720": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qrqvNpFf3GcfVspavhu5qMiAPko4fHit/Karuppu%20(2026)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps)%20-%201.2GB%20-%20ESub.mkv",
-    "link480": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qrqqVLMu3grSeMBPnrYJdCfeE7NHZt1D/Karuppu%20(2026)%20Malayalam%20HQ%20HDRip%20-%20x264%20-%20AAC%20-%20400MB%20-%20ESub.mkv",
-    "telegram": ""
+    "size1080": "3.2 GB",
+    "size720": "1.6 GB",
+    "size480": "400 MB",
+    "telegram": "https://t.me/Movies4U"
   },
   {
     "title": "Asianet",
@@ -87,29 +61,5 @@
     "year": "LIVE",
     "lang": "Channel",
     "rating": "HD"
-  },
-  {
-    "title": "Mazhavil",
-    "link1080": "https://yuppmedtaorire.akamaized.net/v1/master/a0d007312bfd99c47f76b77ae26b1ccdaae76cb1/mazhavilmanorama_nim_https/050522/mazhavilmanorama/playlist.m3u8",
-    "img": "mazhavil",
-    "year": "LIVE",
-    "lang": "Channel",
-    "rating": "HD"
-  },
-  {
-    "title": "Flowers",
-    "link1080": "https://yuppmedtaorire.akamaized.net/v1/master/a0d007312bfd99c47f76b77ae26b1ccdaae76cb1/flowers_nim_https/050522/flowers/playlist.m3u8",
-    "img": "flowers",
-    "year": "LIVE",
-    "lang": "Channel",
-    "rating": "HD"
-  },
-  {
-    "title": "18+",
-    "link1080": "#",
-    "img": "ic_18_plus",
-    "year": "LIVE",
-    "lang": "Channel",
-    "rating": "HD"
   }
-]
+];
