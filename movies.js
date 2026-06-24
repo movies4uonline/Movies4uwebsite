57c4b886585ae08259eac0470826b5036373e4b7
movies4uonline <moviesu537@gmail.com>
2026-06-24 14:54:37 +0530

    Update movies.js

--- movies.js
+++ movies.js
@@ -1,29 +1,16 @@
- [
+[
   {
     "title": "Gu (2024) Malayalam HDRip",
     "year": "2024",
     "lang": "Malayalam",
     "img": "gu",
+    "rating": "8.5",
     "link1080": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qrgt34CccwrlPaxykTExBbfaLsJrGFey/Gu%20(2024)%20Malayalam%20HQ%20HDRip%20-%201080p%20-%20x264%20-%20(DD%2B5.1%20-%20384Kbps%20%26%20AAC%202.0)%20-%202.8GB%20-%20ESub.mkv",
     "link720": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qrgobqJrc6GY8uKdd47FzN1NTbkCkcUA/Gu%20(2024)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.4GB%20-%20ESub.mkv",
     "link480": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qBDljCHwzNJs9WMCCrbGlkmpFxQxPm5b/Gu%20(2024)%20Malayalam%20HQ%20HDRip%20-%20x264%20-%20AAC%20-%20400MB%20-%20ESub.mkv",
-    "link_server2": "https://server2.com/video.mp4",
-    "link_dl1": "https://download-server.com/file.mp4"
-  },
-  {
-    "title": "Gu",
-    "img": "gu",
-    "year": "2024",
-    "lang": "Malayalam",
-    "rating": "0.0",
-    "link_online": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qrgobqJrc6GY8uKdd47FzN1NTbkCkcUA/Gu%20(2024)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.4GB%20-%20ESub.mkv",
-    "link1080": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qrgt34CccwrlPaxykTExBbfaLsJrGFey/Gu%20(2024)%20Malayalam%20HQ%20HDRip%20-%201080p%20-%20x264%20-%20(DD%2B5.1%20-%20384Kbps%20%26%20AAC%202.0)%20-%202.8GB%20-%20ESub.mkv",
-    "size1080": "2.8GB",
-    "link720": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qrgobqJrc6GY8uKdd47FzN1NTbkCkcUA/Gu%20(2024)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.4GB%20-%20ESub.mkv",
-    "size720": "1.4GB",
-    "link480": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qBDljCHwzNJs9WMCCrbGlkmpFxQxPm5b/Gu%20(2024)%20Malayalam%20HQ%20HDRip%20-%20x264%20-%20AAC%20-%20400MB%20-%20ESub.mkv",
-    "size480": "400MB",
-    "telegram": ""
+    "link_server2": "",
+    "link_server3": "https://download-server.com/file.mp4",
+    "telegram": "https://t.me/Movies4U"
   },
   {
     "title": "Athiradi",
@@ -32,11 +19,8 @@
     "lang": "Malayalam",
     "rating": "7.7",
     "link1080": "https://watchmovies4u.movies4uu.workers.dev/dl/1h9zhDkXTnMinD9NV93H2hUx33M7Lhx5Ho5x9oUjldlra24kNdOTB12rswnp5BJneRNbHzvNAJTn/Athiradi%20(2026)%20Malayalam%20HQ%20HDRip%20-%201080p%20-%20x264%20-%20(DD%2B5.1%20ATMOS%20-%20448Kbps%20%26%20AAC%202.0)%20-%203.2GB%20-%20ESub.mkv",
-    "size1080": "3.2GB",
     "link720": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qBDiSNL94ARgjBSX3woKf3J44lKLYYEm/Athiradi%20(2026)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20256Kbps%20%26%20AAC%202.0)%20-%201.6GB%20-%20ESub.mkv",
-    "size720": "1.6GB",
     "link480": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qBDe19So4b6SCW5BVGRSaOz2BDDK7WXM/Athiradi%20(2026)%20Malayalam%20HQ%20HDRip%20-%20x264%20-%20AAC%20-%20400MB%20-%20ESub.mkv",
-    "size480": "400MB",
     "telegram": ""
   },
   {
@@ -46,12 +30,9 @@
     "lang": "Malayalam",
     "rating": "7.4",
     "link1080": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qgTFEapCPETDb45PbbCKu1zOTDejUroz/Drishyam%203%20(2026)%20Malayalam%20TRUE%20WEB-DL%20-%201080p%20-%20AVC%20-%20UNTOUCHED%20-%20(DD%2B5.1%20-%20640Kbps%20%26%20AAC%202.0)%20-%202.5GB%20-%20ESub.mkv",
-    "size1080": "2.5GB",
     "link720": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qgTDdltfks1rkJc9CgSPlMzy2AaK5C0B/Drishyam%203%20(2026)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.4GB%20-%20ESub.mkv",
-    "size720": "1.4GB",
     "link480": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qgTAMwwRPf9fuoiu3m5SIWnP44E43j7s/Drishyam%203%20(2026)%20Malayalam%20HQ%20HDRip%20-%20x264%20-%20AAC%20-%20400MB%20-%20ESub.mkv",
-    "size480": "400MB",
-    "telegram": "",
+    "telegram": ""
   },
   {
     "title": "Karuppu",
@@ -64,40 +45,36 @@
     "link480": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qrqqVLMu3grSeMBPnrYJdCfeE7NHZt1D/Karuppu%20(2026)%20Malayalam%20HQ%20HDRip%20-%20x264%20-%20AAC%20-%20400MB%20-%20ESub.mkv",
     "telegram": ""
   },
-  
   {
     "title": "Asianet",
-    "link": "http://217.20.112.199/asianet/index.m3u8",
-    "img": "",
+    "link1080": "http://217.20.112.199/asianet/index.m3u8",
+    "img": "asianet",
     "year": "LIVE",
     "lang": "Channel",
     "rating": "HD"
   },
   {
     "title": "Mazhavil",
-    "link": "https://yuppmedtaorire.akamaized.net/v1/master/a0d007312bfd99c47f76b77ae26b1ccdaae76cb1/mazhavilmanorama_nim_https/050522/mazhavilmanorama/playlist.m3u8",
-    "img": "",
+    "link1080": "https://yuppmedtaorire.akamaized.net/v1/master/a0d007312bfd99c47f76b77ae26b1ccdaae76cb1/mazhavilmanorama_nim_https/050522/mazhavilmanorama/playlist.m3u8",
+    "img": "mazhavil",
     "year": "LIVE",
     "lang": "Channel",
     "rating": "HD"
   },
   {
     "title": "Flowers",
-    "link": "https://yuppmedtaorire.akamaized.net/v1/master/a0d007312bfd99c47f76b77ae26b1ccdaae76cb1/flowers_nim_https/050522/flowers/playlist.m3u8",
-    "img": "",
+    "link1080": "https://yuppmedtaorire.akamaized.net/v1/master/a0d007312bfd99c47f76b77ae26b1ccdaae76cb1/flowers_nim_https/050522/flowers/playlist.m3u8",
+    "img": "flowers",
     "year": "LIVE",
     "lang": "Channel",
     "rating": "HD"
   },
   {
     "title": "18+",
-    "link": "#",
-    "img": "",
+    "link1080": "#",
+    "img": "ic_18_plus",
     "year": "LIVE",
     "lang": "Channel",
     "rating": "HD"
   }
 ]
-
-
-
