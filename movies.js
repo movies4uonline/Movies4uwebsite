999edaec8f56f2b7deb8d4645f0e6c138ff13ff1
movies4uonline <moviesu537@gmail.com>
2026-06-28 02:26:47 +0530

    Update movies.js

--- movies.js
+++ movies.js
@@ -1,5 +1,31 @@
 [
   {
+    "title": "Blast",
+    "playerTitle": "Blast (2026) Malayalam Full Movie HD",
+    "img": "blast",
+    "year": "2026",
+    "lang": "Malayalam",
+    "quality": "HD",
+    "rating": "7.5",
+    
+    // പ്ലേ ചെയ്യാനും ഇൻ-ആപ്പ് ഡൗൺലോഡിനും ഒരേ ലിങ്കുകൾ
+    "link1080": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qhdL8QvIxYprnDP3vXmng3hbcxDCdkLo/Blast%20(2026)%20Malayalam%20TRUE%20WEB-DL%20-%201080p%20-%20AVC%20-%20UNTOUCHED%20-%20(DD%2B5.1%20-%20192Kbps)%20-%202.2GB%20-%20ESub.mkv",
+    "link720": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qhdGhcCXxyF3GY1Io7PvyuVrW3myjONo/Blast%20(2026)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps)%20-%201.5GB%20-%20ESub.mkv",
+    "link480": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qhdII1zl2LxfxiVnX2zqNtC0cF6OWCFh/Blast%20(2026)%20Malaylam%20HQ%20HDRip%20-%20x264%20-%20AAC%20-%20400MB%20-%20ESub.mkv",
+    
+    // ഫയൽ സൈസ് (ക്വാളിറ്റി വിൻഡോയിൽ കാണിക്കാൻ)
+    "size1080": "2.3 GB",
+    "size720": "1.6 GB",
+    "size480": "400 MB",
+    
+    // എക്സ്റ്റേണൽ ബ്രൗസർ ഡൗൺലോഡിന് പ്രത്യേകം ലിങ്ക്
+    "external_link1080": "https://lcuapi.onlinewatch.workers.dev/api/fdl/AgAD2S4790469",
+    "external_link720": "https://lcuapi.onlinewatch.workers.dev/api/fdl/AgAD1y4790467",
+    "external_link480": "https://lcuapi.onlinewatch.workers.dev/api/fdl/AgADyS4790465",
+    
+    "telegram": "https://t.me/Mamitha_M4U_bot?start=getfile-Blast"
+  },
+  {
     "title": "Revolver Rinko",
     "playerTitle": "Revolver Rinko (2026) Malayalam Full Movie HD",
     "img": "revolverrinko",
