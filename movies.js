8fe3b53a0b56054cb013482df9b5f2d2fecae100
movies4uonline <moviesu537@gmail.com>
2026-06-28 02:09:03 +0530

    Update movies.js

--- movies.js
+++ movies.js
@@ -1,29 +1,29 @@
 [
   {
-    "title": "Athiradi",
-    "playerTitle": "Athiradi (2026) Malayalam Full Movie HD",
-    "img": "athiradi",
+    "title": "Revolver Rinko",
+    "playerTitle": "Revolver Rinko (2026) Malayalam Full Movie HD",
+    "img": "RevolverRinko",
     "year": "2026",
     "lang": "Malayalam",
     "quality": "HD",
-    "rating": "7.7",
+    "rating": "8.6",
     
     // പ്ലേ ചെയ്യാനും ഇൻ-ആപ്പ് ഡൗൺലോഡിനും ഒരേ ലിങ്കുകൾ
-    "link1080": "https://server.com/1080p.mkv",
-    "link720": "https://server.com/720p.mkv",
-    "link480": "https://server.com/480p.mkv",
+    "link1080": "https://lcuapi.onlinewatch.workers.dev/api/fdl/AgADuy4790461",
+    "link720": "https://lcuapi.onlinewatch.workers.dev/api/fdl/AgADpS4790460",
+    "link480": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qBXjvPYtMuCGPvOQgszZcSN7TbjAaIRt/Revolver%20Rinko%20(2026)%20Malayalam%20HQ%20HDRip%20-%20x264%20-%20AAC%20-%20400MB%20-%20ESub.mkv",
     
     // ഫയൽ സൈസ് (ക്വാളിറ്റി വിൻഡോയിൽ കാണിക്കാൻ)
-    "size1080": "2.1 GB",
-    "size720": "1.1 GB",
-    "size480": "550 MB",
+    "size1080": "2.5 GB",
+    "size720": "1.2 GB",
+    "size480": "400 MB",
     
     // എക്സ്റ്റേണൽ ബ്രൗസർ ഡൗൺലോഡിന് പ്രത്യേകം ലിങ്ക്
-    "external_link1080": "https://external.com/dl/1080.mkv",
-    "external_link720": "https://external.com/dl/720.mkv",
-    "external_link480": "https://external.com/dl/480.mkv",
+    "external_link1080": "https://lcuapi.onlinewatch.workers.dev/api/fdl/AgADuy4790461",
+    "external_link720": "https://lcuapi.onlinewatch.workers.dev/api/fdl/AgADuy4790461",
+    "external_link480": "https://lcuapi.onlinewatch.workers.dev/api/fdl/AgADli4790459",
     
-    "telegram": "https://t.me/movies4u_links/1"
+    "telegram": "https://t.me/Mamitha_M4U_bot?start=getfile-Revolver%20Rinko"
   },
   {
     "title": "Asianet",
