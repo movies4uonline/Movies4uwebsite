d9a014c490ab3a20f719cd830f9897436ec435cf
movies4uonline <moviesu537@gmail.com>
2026-06-29 08:43:36 +0530

    Update movies.js

--- movies.js
+++ movies.js
@@ -50,6 +50,32 @@
     "external_link480": "https://lcuapi.onlinewatch.workers.dev/api/fdl/AgADli4790459",
     
     "telegram": "https://t.me/M4Upremium_bot?start=BATCH-MTg4NDY"
+    },
+  {
+    "title": "Athiradi",
+    "playerTitle": "Athiradi (2026) Malayalam Full Movie HD",
+    "img": "athiradi",
+    "year": "2026",
+    "lang": "Malayalam",
+    "quality": "HD",
+    "rating": "7.0",
+    
+    // പ്ലേ ചെയ്യാനും ഇൻ-ആപ്പ് ഡൗൺലോഡിനും ഒരേ ലിങ്കുകൾ
+    "link1080": "https://watchmovies4u.movies4uu.workers.dev/dl/1h9zhDkXTnMinD9NV93H2hUx33M7Lhx5Ho5x9oUjldlra24kNdOTB12rswnp5BJneRNbHzvNAJTn/Athiradi%20(2026)%20Malayalam%20HQ%20HDRip%20-%201080p%20-%20x264%20-%20(DD%2B5.1%20ATMOS%20-%20448Kbps%20%26%20AAC%202.0)%20-%203.2GB%20-%20ESub.mkv",
+    "link720": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qhdDQnGA2lMRQD82Pd2z0cR1CbiwSzd9/Athiradi%20(2026)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20HEVC%20-%20x265%20-%20(DD%2B5.1%20-%20256Kbps%20%26%20AAC%202.0)%20-%201.2GB%20-%20ESub.mkv",
+    "link480": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qBDe19So4b6SCW5BVGRSaOz2BDDK7WXM/Athiradi%20(2026)%20Malayalam%20HQ%20HDRip%20-%20x264%20-%20AAC%20-%20400MB%20-%20ESub.mkv",
+    
+    // ഫയൽ സൈസ് (ക്വാളിറ്റി വിൻഡോയിൽ കാണിക്കാൻ)
+    "size1080": "3.3 GB",
+    "size720": "1.3 GB",
+    "size480": "400 MB",
+    
+    // എക്സ്റ്റേണൽ ബ്രൗസർ ഡൗൺലോഡിന് പ്രത്യേകം ലിങ്ക്
+    "external_link1080": "https://lcuapi.onlinewatch.workers.dev/api/fdl/AgADei4791736",
+    "external_link720": "https://lcuapi.onlinewatch.workers.dev/api/fdl/AgADfB4791735",
+    "external_link480": "https://lcuapi.onlinewatch.workers.dev/api/fdl/AgADeh4791733",
+    
+    "telegram": "https://t.me/M4Upremium_bot?start=BATCH-MTg4NTI"
   },
   {
     "title": "Asianet",
