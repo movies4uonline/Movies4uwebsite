42909ad79e06935d5522ccc21158a2a9aec9a29c
movies4uonline <moviesu537@gmail.com>
2026-07-10 02:29:15 +0530

    Update movies.js

--- movies.js
+++ movies.js
@@ -8,22 +8,16 @@
     "lang": "Malayalam",
     "quality": "HD",
     "rating": "5.9",
-    
-    // പ്ലേ ചെയ്യാനും ഇൻ-ആപ്പ് ഡൗൺലോഡിനും ഒരേ ലിങ്കുകൾ
     "link1080": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6wa7pLk9GByYSXNKzh4I8kZWoJV31LbASRT/Peddi%20(2026)%20Malayalam%20TRUE%20WEB-DL%20-%201080p%20-%20AVC%20-%20UNTOUCHED%20-%20(DD%2B5.1%20-%20640Kbps%20%26%20AAC%202.0)%20-%203.5GB%20-%20ESub.mkv",
     "link720": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6wa7pLhIRFbtG5BUenp9dzyX7DhLJtXUAnf/Peddi%20(2026)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.7GB%20-%20ESub.mkv",
     "link480": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6wa7pLcRdMqtgledyA41o1bs7D1FZ45Llt3/Peddi%20(2026)%20Malayalam%20HQ%20HDRip%20-%20x264%20-%20AAC%20-%20550MB%20-%20ESub.mkv",
-    "subtitles": "",
-    // ഫയൽ സൈസ് (ക്വാളിറ്റി വിൻഡോയിൽ കാണിക്കാൻ)
+    "subtitles": [],
     "size1080": "3.5 GB",
     "size720": "1.7 GB",
     "size480": "550 MB",
-    
-    // എക്സ്റ്റേണൽ ബ്രൗസർ ഡൗൺലോഡിന് പ്രത്യേകം ലിങ്ക്
     "external_link1080": "https://online.movies4uu.workers.dev/dl/AgAD7x2083",
     "external_link720": "https://online.movies4uu.workers.dev/dl/AgAD5h2082",
     "external_link480": "https://online.movies4uu.workers.dev/dl/AgADYy2060",
-    
     "telegram": "https://t.me/M4Upremium_bot?start=BATCH-MTg5MDI"
   },
   {
@@ -35,22 +29,15 @@
     "lang": "Hindi",
     "quality": "HD",
     "rating": "7.5",
-    
-    // പ്ലേ ചെയ്യാനും ഇൻ-ആപ്പ് ഡൗൺലോഡിനും ഒരേ ലിങ്കുകൾ
     "link1080": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6wa751zCAqUeknB50MW93fcltLGsFhX1FbX/Chand%20Mera%20Dil%20(2026)%20Hindi%20HQ%20HDRip%20-%201080p%20-%20x264%20-%20(DD5.1%20-%20384Kbps%20%26%20AAC%202.0)%20-%202.9GB%20-%20ESub.mkv",
     "link720": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6wa751EuejFeK7YLGAhgSM3RFB4jg4CSIx3/Chand%20Mera%20Dil%20(2026)%20Hindi%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.6GB%20-%20ESub.mkv",
     "link480": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6waO4aBIyCsrjVFpyYVbPlSByk0Od1WvGcL/Chand%20Mera%20Dil%20(2026)%20Hindi%20HQ%20HDRip%20-%20x264%20-%20AAC%20-%20400MB%20-%20ESub.mkv",
-    
-    // ഫയൽ സൈസ് (ക്വാളിറ്റി വിൻഡോയിൽ കാണിക്കാൻ)
     "size1080": "2.9 GB",
     "size720": "1.6 GB",
     "size480": "400 MB",
-    
-    // എക്സ്റ്റേണൽ ബ്രൗസർ ഡൗൺലോഡിന് പ്രത്യേകം ലിങ്ക്
     "external_link1080": "https://online.movies4uu.workers.dev/dl/AgADnB1927",
     "external_link720": "https://online.movies4uu.workers.dev/dl/AgADmR1924",
     "external_link480": "https://online.movies4uu.workers.dev/dl/AgADhx1923",
-    
     "telegram": "https://t.me/M4Upremium_bot?start=BATCH-MTg4OTk"
   },
   {
@@ -62,23 +49,13 @@
     "lang": "English",
     "quality": "HD",
     "rating": "7.2",
-    
-    // പ്ലേ ചെയ്യാനും ഇൻ-ആപ്പ് ഡൗൺലോഡിനും ഒരേ ലിങ്കുകൾ
     "link1080": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6waO4awQUJHqUbhITbA3ZP117r1KOQtGfDL/The%20Furious%20(2026)%20English%20HQ%20HDRip%20-%201080p%20-%20x264%20-%20(DD%2B5.1%20-%20640Kbps%20%26%20AAC%202.0)%20-%202.3GB.mkv",
     "link720": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6waOextZ6HMOBdLKldZtmT1MEGjvF0BMczz/The%20Furious%20(2026)%20English%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.1GB.mkv",
-    
-    
-    // ഫയൽ സൈസ് (ക്വാളിറ്റി വിൻഡോയിൽ കാണിക്കാൻ)
     "size1080": "2.3 GB",
     "size720": "1.1 GB",
-    
-    
-    // എക്സ്റ്റേണൽ ബ്രൗസർ ഡൗൺലോഡിന് പ്രത്യേകം ലിങ്ക്
     "external_link1080": "https://online.movies4uu.workers.dev/dl/AgADeR1922",
     "external_link720": "https://online.movies4uu.workers.dev/dl/AgADfR1921",
-    
-    
-    "telegram": "https://t.me/M4Upremium_bot?start=BATCH-MTg4OTM"
+    "telegram": "https://t.me/M4Upremium_bot?start=BATCH-MTg4OT3"
   },
   {
     "title": "Mollywood Times",
@@ -89,22 +66,15 @@
     "lang": "Malayalam",
     "quality": "HD",
     "rating": "7.4",
-    
-    // പ്ലേ ചെയ്യാനും ഇൻ-ആപ്പ് ഡൗൺലോഡിനും ഒരേ ലിങ്കുകൾ
     "link1080": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6waO3QwdSwmJ0pRcZfGR3FhlEVXOUHrdltw/Mollywood%20Times%20(2026)%20Malayalam%20HQ%20HDRip%20-%201080p%20-%20HEVC%20-%20x265%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%202.3GB%20-%20ESub.mkv",
     "link720": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6waO3QrmeDBIAFtwjslJe8ppfmHbvpNVmH4/Mollywood%20Times%20(2026)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.8GB%20-%20ESub.mkv",
     "link480": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6waOedouqBH6hHXxLuL8BaUEDmKHTFyy7Gu/Mollywood%20Times%20(2026)%20Malayalam%20HQ%20HDRip%20-%20x264%20-%20AAC%20-%20400MB%20-%20ESub.mkv",
-    
-    // ഫയൽ സൈസ് (ക്വാളിറ്റി വിൻഡോയിൽ കാണിക്കാൻ)
     "size1080": "2.3 GB",
     "size720": "1.7 GB",
     "size480": "400 MB",
-    
-    // എക്സ്റ്റേണൽ ബ്രൗസർ ഡൗൺലോഡിന് പ്രത്യേകം ലിങ്ക്
     "external_link1080": "https://online.movies4uu.workers.dev/dl/AgADCy1346",
     "external_link720": "https://online.movies4uu.workers.dev/dl/AgAD6S1345",
     "external_link480": "https://online.movies4uu.workers.dev/dl/AgADyy1344",
-    
     "telegram": "https://t.me/M4Upremium_bot?start=BATCH-MTg4ODU"
   },
   {
@@ -116,22 +86,15 @@
     "lang": "Malayalam",
     "quality": "HD",
     "rating": "9.0",
-    
-    // പ്ലേ ചെയ്യാനും ഇൻ-ആപ്പ് ഡൗൺലോഡിനും ഒരേ ലിങ്കുകൾ
     "link1080": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6waOedm3BFjB4PLHqB5zGptR3yCMLYC7Y4C/Karakkam%20(2026)%20Malayalam%20HQ%20HDRip%20-%201080p%20-%20x264%20-%20(DD%2B5.1%20ATMOS%20-%20448Kbps%20%26%20AAC%202.0)%20-%202.5GB%20-%20ESub.mkv",
     "link720": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6waNTtDXkr2lJ7KSd0Czw6CwZFdtiaTrOCJ/Karakkam%20(2026)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.2GB%20-%20ESub.mkv",
     "link480": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6waNTtGo9npQVZWIxUi8qS3mlxoRmEV3bnT/Karakkam%20(2026)%20Malayalam%20HQ%20HDRip%20-%20x264%20-%20AAC%20-%20400MB%20-%20ESub.mkv",
-    
-    // ഫയൽ സൈസ് (ക്വാളിറ്റി വിൻഡോയിൽ കാണിക്കാൻ)
     "size1080": "2.5 GB",
     "size720": "1.2 GB",
     "size480": "400 MB",
-    
-    // എക്സ്റ്റേണൽ ബ്രൗസർ ഡൗൺലോഡിന് പ്രത്യേകം ലിങ്ക്
     "external_link1080": "https://online.movies4uu.workers.dev/dl/AgADki1339",
     "external_link720": "https://online.movies4uu.workers.dev/dl/AgADiy1332",
     "external_link480": "https://online.movies4uu.workers.dev/dl/AgADfi1331",
-    
     "telegram": "https://t.me/M4Upremium_bot?start=BATCH-MTg4ODM"
   },
   {
@@ -143,165 +106,123 @@
     "lang": "English",
     "quality": "HD",
     "rating": "8.1",
-    
-    // പ്ലേ ചെയ്യാനും ഇൻ-ആപ്പ് ഡൗൺലോഡിനും ഒരേ ലിങ്കുകൾ
     "link1080": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6waNTtBwvuEQwfz1S6X0Blbx0vTBjvFK9Es/Obsession%20(2026)%20English%20HQ%20HDRip%20-%201080p%20-%20x264%20-%20(DD%2B5.1%20-%20640Kbps)%20-%202.3GB.mkv",
     "link720": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6waO40yYdzpAaaLjh7j1rwl4s6UCytjFyMI/Obsession%20(2026)%20English%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps)%20-%201.7GB.mkv",
     "link480": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6waO40u6zGEzKqnCBjXTBZt8Jiud0iGe8Ua/Obsession%20(2026)%20English%20HQ%20HDRip%20-%20x264%20-%20(AAC%202.0)%20-%20400MB.mkv",
-    
-    // ഫയൽ സൈസ് (ക്വാളിറ്റി വിൻഡോയിൽ കാണിക്കാൻ)
     "size1080": "2.3 GB",
     "size720": "1.7 GB",
     "size480": "400 MB",
-    
-    // എക്സ്റ്റേണൽ ബ്രൗസർ ഡൗൺലോഡിന് പ്രത്യേകം ലിങ്ക്
     "external_link1080": "https://online.movies4uu.workers.dev/dl/AgADZy1131",
     "external_link720": "https://online.movies4uu.workers.dev/dl/AgAD4C1118",
     "external_link480": "https://online.movies4uu.workers.dev/dl/AgAD3S1115",
-    
     "telegram": "https://t.me/M4Upremium_bot?start=BATCH-MTg4Njk"
   },
   {
     "title": "Blast",
     "playerTitle": "Blast (2026) Malayalam Full Movie HD",
-    "img": "blast",
+    "img": "https://image.tmdb.org/t/p/original/676QmhjH/et00500207-zncraymnbq-landscape.jpg",
     "tvLandscape": "https://i.ibb.co/676QmhjH/et00500207-zncraymnbq-landscape.jpg",
     "year": "2026",
     "lang": "Malayalam",
     "quality": "HD",
     "rating": "7.5",
-    
-    // പ്ലേ ചെയ്യാനും ഇൻ-ആപ്പ് ഡൗൺലോഡിനും ഒരേ ലിങ്കുകൾ
     "link1080": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qhdL8QvIxYprnDP3vXmng3hbcxDCdkLo/Blast%20(2026)%20Malayalam%20TRUE%20WEB-DL%20-%201080p%20-%20AVC%20-%20UNTOUCHED%20-%20(DD%2B5.1%20-%20192Kbps)%20-%202.2GB%20-%20ESub.mkv",
     "link720": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qhdGhcCXxyF3GY1Io7PvyuVrW3myjONo/Blast%20(2026)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps)%20-%201.5GB%20-%20ESub.mkv",
     "link480": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qhdII1zl2LxfxiVnX2zqNtC0cF6OWCFh/Blast%20(2026)%20Malaylam%20HQ%20HDRip%20-%20x264%20-%20AAC%20-%20400MB%20-%20ESub.mkv",
-    
-    // ഫയൽ സൈസ് (ക്വാളിറ്റി വിൻഡോയിൽ കാണിക്കാൻ)
     "size1080": "2.3 GB",
     "size720": "1.6 GB",
     "size480": "400 MB",
-    
-    // എക്സ്റ്റേണൽ ബ്രൗസർ ഡൗൺലോഡിന് പ്രത്യേകം ലിങ്ക്
     "external_link1080": "https://online.movies4uu.workers.dev/dl/AgAD2S1029",
     "external_link720": "https://online.movies4uu.workers.dev/dl/AgAD1y1028",
     "external_link480": "https://online.movies4uu.workers.dev/dl/AgADyS1027",
-    
     "telegram": "https://t.me/M4Upremium_bot?start=BATCH-MTg4NTA"
   },
   {
     "title": "Revolver Rinko",
     "playerTitle": "Revolver Rinko (2026) Malayalam Full Movie HD",
-    "img": "revolverrinko",
+    "img": "https://image.tmdb.org/t/p/original/1YqWZY6R/et00498339-asgmhtrqwj-landscape.jpg",
     "tvLandscape": "https://i.ibb.co/1YqWZY6R/et00498339-asgmhtrqwj-landscape.jpg",
     "year": "2026",
     "lang": "Malayalam",
     "quality": "HD",
     "rating": "8.6",
-    
-    // പ്ലേ ചെയ്യാനും ഇൻ-ആപ്പ് ഡൗൺലോഡിനും ഒരേ ലിങ്കുകൾ
     "link1080": "https://watchmovies4u.movies4uu.workers.dev/dl/1h9zhDkXTnMinD9NV93H2hUx33M7Lhx5Ho5x9oUjldlramO2Tv3aQmKshJXS5M2esTSm98jeNjby/Revolver%20Rinko%20(2026)%20Malayalam%20TRUE%20WEB-DL%20-%201080p%20-%20AVC%20-%20UNTOUCHED%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%202.5GB%20-%20ESub.mkv",
     "link720": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6waOe3erCJvJvktUsW8ht1ya62cfBp0z2sa/Revolver%20Rinko%20(2026)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.3GB%20-%20ESub.mkv",
     "link480": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qBXjvPYtMuCGPvOQgszZcSN7TbjAaIRt/Revolver%20Rinko%20(2026)%20Malayalam%20HQ%20HDRip%20-%20x264%20-%20AAC%20-%20400MB%20-%20ESub.mkv",
-    
-    // ഫയൽ സൈസ് (ക്വാളിറ്റി വിൻഡോയിൽ കാണിക്കാൻ)
     "size1080": "2.5 GB",
     "size720": "1.2 GB",
     "size480": "400 MB",
-    
-    // എക്സ്റ്റേണൽ ബ്രൗസർ ഡൗൺലോഡിന് പ്രത്യേകം ലിങ്ക്
     "external_link1080": "https://online.movies4uu.workers.dev/dl/AgADuy992",
     "external_link720": "https://online.movies4uu.workers.dev/dl/AgADpS991",
     "external_link480": "https://online.movies4uu.workers.dev/dl/AgADli990",
-    
     "telegram": "https://t.me/M4Upremium_bot?start=BATCH-MTg4NDY"
-    },
+  },
   {
     "title": "Athiradi",
     "playerTitle": "Athiradi (2026) Malayalam Full Movie HD",
-    "img": "athiradi",
+    "img": "https://image.tmdb.org/t/p/original/htc7NsL/et00488272-yzuhtwtldt-landscape.jpg",
     "tvLandscape": "https://i.ibb.co/htc7NsL/et00488272-yzuhtwtldt-landscape.jpg",
     "year": "2026",
     "lang": "Malayalam",
     "quality": "HD",
     "rating": "7.0",
-    
-    // പ്ലേ ചെയ്യാനും ഇൻ-ആപ്പ് ഡൗൺലോഡിനും ഒരേ ലിങ്കുകൾ
     "link1080": "https://watchmovies4u.movies4uu.workers.dev/dl/1h9zhDkXTnMinD9NV93H2hUx33M7Lhx5Ho5x9oUjldlra24kNdOTB12rswnp5BJneRNbHzvNAJTn/Athiradi%20(2026)%20Malayalam%20HQ%20HDRip%20-%201080p%20-%20x264%20-%20(DD%2B5.1%20ATMOS%20-%20448Kbps%20%26%20AAC%202.0)%20-%203.2GB%20-%20ESub.mkv",
     "link720": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qhdDQnGA2lMRQD82Pd2z0cR1CbiwSzd9/Athiradi%20(2026)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20HEVC%20-%20x265%20-%20(DD%2B5.1%20-%20256Kbps%20%26%20AAC%202.0)%20-%201.2GB%20-%20ESub.mkv",
     "link480": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qBDe19So4b6SCW5BVGRSaOz2BDDK7WXM/Athiradi%20(2026)%20Malayalam%20HQ%20HDRip%20-%20x264%20-%20AAC%20-%20400MB%20-%20ESub.mkv",
-    
-    // ഫയൽ സൈസ് (ക്വാളിറ്റി വിൻഡോയിൽ കാണിക്കാൻ)
     "size1080": "3.3 GB",
     "size720": "1.3 GB",
     "size480": "400 MB",
-    
-    // എക്സ്റ്റേണൽ ബ്രൗസർ ഡൗൺലോഡിന് പ്രത്യേകം ലിങ്ക്
     "external_link1080": "https://lcuapi.onlinewatch.workers.dev/api/fdl/AgADei4791736",
     "external_link720": "https://lcuapi.onlinewatch.workers.dev/api/fdl/AgADfB4791735",
     "external_link480": "https://lcuapi.onlinewatch.workers.dev/api/fdl/AgADeh4791733",
-    
     "telegram": "https://t.me/M4Upremium_bot?start=BATCH-MTg4NTI"
   },
   {
     "title": "Gu",
     "playerTitle": "Gu (2024) Malayalam Full Movie HD",
-    "img": "gu",
+    "img": "https://image.tmdb.org/t/p/original/JDYDycR/et00397650-apyhgmlgqq-landscape.jpg",
     "tvLandscape": "https://i.ibb.co/JDYDycR/et00397650-apyhgmlgqq-landscape.jpg",
     "year": "2024",
     "lang": "Malayalam",
     "quality": "HD",
     "rating": "0.0",
-    
-    // പ്ലേ ചെയ്യാനും ഇൻ-ആപ്പ് ഡൗൺലോഡിനും ഒരേ ലിങ്കുകൾ
     "link1080": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qrgt34CccwrlPaxykTExBbfaLsJrGFey/Gu%20(2024)%20Malayalam%20HQ%20HDRip%20-%201080p%20-%20x264%20-%20(DD%2B5.1%20-%20384Kbps%20%26%20AAC%202.0)%20-%202.8GB%20-%20ESub.mkv",
     "link720": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qrgobqJrc6GY8uKdd47FzN1NTbkCkcUA/Gu%20(2024)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.4GB%20-%20ESub.mkv",
     "link480": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qBDljCHwzNJs9WMCCrbGlkmpFxQxPm5b/Gu%20(2024)%20Malayalam%20HQ%20HDRip%20-%20x264%20-%20AAC%20-%20400MB%20-%20ESub.mkv",
-    
-    // ഫയൽ സൈസ് (ക്വാളിറ്റി വിൻഡോയിൽ കാണിക്കാൻ)
     "size1080": "2.7 GB",
     "size720": "1.4 GB",
     "size480": "400 MB",
-    
-    // എക്സ്റ്റേണൽ ബ്രൗസർ ഡൗൺലോഡിന് പ്രത്യേകം ലിങ്ക്
     "external_link1080": "https://lcuapi.onlinewatch.workers.dev/api/fdl/AgADOy4791751",
     "external_link720": "https://lcuapi.onlinewatch.workers.dev/api/fdl/AgADJy4791750",
     "external_link480": "https://lcuapi.onlinewatch.workers.dev/api/fdl/AgADHi4791748",
-    
     "telegram": "https://t.me/M4Upremium_bot?start=BATCH-MTg4NTQ"
   },
   {
     "title": "Drishyam 3",
     "playerTitle": "Drishyam 3 (2026) Malayalam Full Movie HD",
-    "img": "drishyam3",
+    "img": "https://image.tmdb.org/t/p/original/zHRDtQ4H/et00487295-vaweahqtyh-landscape.jpg",
     "tvLandscape": "https://i.ibb.co/zHRDtQ4H/et00487295-vaweahqtyh-landscape.jpg",
     "year": "2026",
     "lang": "Malayalam",
     "quality": "HD",
     "rating": "6.7",
-    
-    // പ്ലേ ചെയ്യാനും ഇൻ-ആപ്പ് ഡൗൺലോഡിനും ഒരേ ലിങ്കുകൾ
     "link1080": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qgTFEapCPETDb45PbbCKu1zOTDejUroz/Drishyam%203%20(2026)%20Malayalam%20TRUE%20WEB-DL%20-%201080p%20-%20AVC%20-%20UNTOUCHED%20-%20(DD%2B5.1%20-%20640Kbps%20%26%20AAC%202.0)%20-%202.5GB%20-%20ESub.mkv",
     "link720": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qgTDdltfks1rkJc9CgSPlMzy2AaK5C0B/Drishyam%203%20(2026)%20Malayalam%20HQ%20HDRip%20-%20720p%20-%20x264%20-%20(DD%2B5.1%20-%20192Kbps%20%26%20AAC%202.0)%20-%201.4GB%20-%20ESub.mkv",
     "link480": "https://watchmovies4u.movies4uu.workers.dev/dl/5gPvGOqziGbpTzGu7XpfHrXqsDAI3eoDx9sQSOQvRB6w9qgTAMwwRPf9fuoiu3m5SIWnP44E43j7s/Drishyam%203%20(2026)%20Malayalam%20HQ%20HDRip%20-%20x264%20-%20AAC%20-%20400MB%20-%20ESub.mkv",
-    
-    // ഫയൽ സൈസ് (ക്വാളിറ്റി വിൻഡോയിൽ കാണിക്കാൻ)
     "size1080": "2.5 GB",
     "size720": "1.4 GB",
     "size480": "400 MB",
-    
-    // എക്സ്റ്റേണൽ ബ്രൗസർ ഡൗൺലോഡിന് പ്രത്യേകം ലിങ്ക്
     "external_link1080": "https://lcuapi.onlinewatch.workers.dev/api/fdl/AgADaR4791761",
     "external_link720": "https://lcuapi.onlinewatch.workers.dev/api/fdl/AgAD3h4791758",
     "external_link480": "https://lcuapi.onlinewatch.workers.dev/api/fdl/AgADUR4791757",
-    
     "telegram": "https://t.me/M4Upremium_bot?start=BATCH-MTg4NTY"
   },
   {
     "title": "Asianet",
     "link1080": "http://217.20.112.199/asianet/index.m3u8",
-    "img": "asianet",
+    "img": "https://i.ibb.co/ZzXyXzJ/asianet-logo.png",
     "year": "LIVE",
     "lang": "Channel",
     "rating": "HD"
   }
-];
+]
