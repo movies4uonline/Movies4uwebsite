ade22f1dc8f026d653d73775e44a0fc0236fd486
movies4uonline <moviesu537@gmail.com>
2026-07-13 23:54:27 +0530

    Update and rename series.json to series.js

---
+++ series.js
@@ -0,0 +1,21 @@
+[
+  {
+    "title": "Second Love",
+    "img": "https://i.ibb.co/3946d0F4/FB-IMG-1783966374993.jpg",
+    "tvLandscape": "https://i.ibb.co/Q3pgMmG2/images-9.jpg",
+    "year": "2026",
+    "lang": "Malayalam",
+    "rating": "0.0",
+    "description": "",
+    "episodes": [
+      {
+        "name": "Second Love (2026) S01 EP01 [Tam + Tel + Hin + Mal + Kan] HD",
+        "url1080": "https://your_video_link_1080p_1",
+        "url720": "http://watchmovies4u.movies4uu.workers.dev/dl/6a552b28db4519c3e742e5a9",
+        "extUrl1080": "https://external_download_link_1"
+        "extUrl720": "http://watchmovies4u.movies4uu.workers.dev/dl/6a552b28db4519c3e742e5a9"
+      }   
+    ]
+  }
+]
+
