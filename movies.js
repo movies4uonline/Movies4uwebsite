4c85d6426f33ee5dd7724beaf3659b1e7cbb2af6
movies4uonline <moviesu537@gmail.com>
2026-08-31 16:37:42 +0530

    Update movies.js

--- movies.js
+++ movies.js
@@ -1,29 +1,3 @@
 [
-    {
-        "title": "bdbdhd",
-        "img": "https:\/\/watchmovies1.movies4uu.workers.dev\/dl\/AgADQy10428",
-        "playerTitle": "",
-        "tvLandscape": "https:\/\/watchmovies1.movies4uu.workers.dev\/dl\/AgADQy10428",
-        "year": "2026",
-        "rating": "8.5",
-        "lang": "Malayalam",
-        "quality": "HD",
-        "duration": "",
-        "description": "",
-        "url1080": "",
-        "url720": "",
-        "url480": "",
-        "extUrl1080": "",
-        "extUrl720": "",
-        "extUrl480": "",
-        "telegram": "",
-        "telegram1080": "",
-        "telegram720": "",
-        "telegram480": "",
-        "subtitleUrl": "",
-        "trailerUrl": "",
-        "isSeries": false,
-        "episodesJson": "",
-        "timestamp": -1788174351674
-    }
+
 ]
