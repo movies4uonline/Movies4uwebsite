35581b6e9f3fcf24c608d420b7890c10d925a329
movies4uonline <moviesu537@gmail.com>
2026-08-31 21:35:46 +0530

    Admin: Updated movies.js

--- movies.js
+++ movies.js
@@ -1,9 +1,9 @@
 [
     {
         "title": "Jana Nayagan",
-        "img": "https:\/\/watchmovies1.movies4uu.workers.dev\/dl\/AgADlC10492",
-        "playerTitle": "https:\/\/watchmovies1.movies4uu.workers.dev\/dl\/AgADlC10492",
-        "tvLandscape": "",
+        "img": "https:\/\/filetolinkv5.sumandevisgh63.workers.dev\/\/dl\/401249?code=67b260f85f364d8691",
+        "playerTitle": "",
+        "tvLandscape": "https:\/\/filetolinkv5.sumandevisgh63.workers.dev\/\/dl\/401249?code=67b260f85f364d8691293b",
         "year": "2026",
         "rating": "8.5",
         "lang": "Malayalam",
@@ -11,7 +11,7 @@
         "duration": "",
         "description": "",
         "url1080": "",
-        "url720": "https:\/\/watchmovies1.movies4uu.workers.dev\/dl\/AgADlC10492",
+        "url720": "https:\/\/watchmovies1.movies4uu.workers.dev\/dl\/AgADXD10494",
         "url480": "",
         "extUrl1080": "",
         "extUrl720": "",
@@ -24,6 +24,6 @@
         "trailerUrl": "",
         "isSeries": false,
         "episodesJson": "",
-        "timestamp": -1788191494147
+        "timestamp": -1788192342306
     }
 ]
