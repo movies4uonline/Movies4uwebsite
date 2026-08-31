3e1f02c0d9e894e344149749a1b60a32c2a73c9d
movies4uonline <moviesu537@gmail.com>
2026-08-31 16:32:35 +0530

    Update movies.js

--- movies.js
+++ movies.js
@@ -1,24 +1,3 @@
 [
-{
-    "title": "hdhdhd",
-    "img": "https:\/\/watchmovies1.movies4uu.workers.dev\/dl\/AgADrB10405",
-    "url1080": "",
-    "url720": "",
-    "url480": "",
-    "extUrl1080": "",
-    "extUrl720": "",
-    "extUrl480": "",
-    "year": "2026",
-    "lang": "Malayalam",
-    "quality": "HD",
-    "rating": "8.5",
-    "duration": "",
-    "description": "",
-    "tvLandscape": "https:\/\/watchmovies1.movies4uu.workers.dev\/dl\/AgADrB10405",
-    "trailerUrl": "",
-    "telegram": "",
-    "telegram1080": "",
-    "telegram720": "",
-    "telegram480": ""
-},
+
 ]
